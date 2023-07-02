3c969f72b7e6f817a4833c8512215cb14ccedec7
sp202305 <shpen2005@163.com>
2023-07-02 20:34:22 +0800

    test

--- sdk/user/emf_ad15n_hal/hal_pwm.c
+++ sdk/user/emf_ad15n_hal/hal_pwm.c
@@ -35,7 +35,17 @@
 /******************************************************************************************************
 **	static Parameters
 *******************************************************************************************************/
+typedef struct __HAL_PWM__
+{
+	u32	freq;
+	u16	pwm;	//
+	u8	output;	//0xFF不使用output,用硬件IO;0-1:使用映射IO
+	u8  timer;	//哪个timer
+	u8  inv;	//反向
+	u8	io;		//IO
+}HAL_MCPWM;
 
+HAL_MCPWM	gLEDPWM[4];
 /******************************************************************************************************
 **	public Parameters
 *******************************************************************************************************/
@@ -79,20 +89,61 @@ const u32 mcpwm_tab[4] = {
     gpio_set_direction(mcpwm_tab[channel], 0);      \
 	JL_IOMC->IOMC0 |= BIT(16+channel);
 
+u8	mcpwm_att(u8 channel)
+{
+    if (m_pwm_map[channel].pin == mcpwm_tab[channel]) 
+    {
+        JL_IOMC->IOMC0|=1<<(16+channel);
+        gpio_set_direction(m_pwm_map[channel].pin,0);		
+        gpio_set_die(m_pwm_map[channel].pin,1);	
+        gpio_set_pull_up(m_pwm_map[channel].pin,0);
+        gpio_set_pull_down(m_pwm_map[channel].pin,0);	
+        printf("Hard PWM\n");		
+    }
+    else{
+        // if((pwm->timer&0x01)==0)
+		// 	out=0;
+		// else
+		// 	out=1;
+		// die=pwm->output;
+		// out<<=die*3;
+		// JL_IOMC->IOMC3&=~(0x07<<(die*3));
+		// JL_IOMC->IOMC3|=out;	
+		// gpio_set_direction(m_pwm_map[channel].pin,0);
+		// gpio_set_pull_up(m_pwm_map[channel].pin,1);
+		// gpio_set_pull_down(m_pwm_map[channel].pin,1);	
+		// gpio_write(m_pwm_map[channel].pin,out);			
+		// gpio_set_die(m_pwm_map[channel].pin,1);	
+        if(channel <= 1)
+        {
+            gpio_output_channle(m_pwm_map[channel].pin, CH0_CH0_PWM+channel);//io选择--->设置输出通道
+        }
+        else{
+            gpio_output_channle(m_pwm_map[channel].pin, CH1_CH2_PWM+channel-2);//io选择--->设置输出通道
+        }   
+    }
+	printf("iomc0:%x,iomc3:%x\n",JL_IOMC->IOMC0,JL_IOMC->IOMC3);
+	return true;
+}
+
 static void mcpwm_set(u8 channel, u32 fre, u32 duty)
 {
     switch (channel) {
     case 0:
         _mcpwm_set(0, fre, duty);
+        mcpwm_att(0);
         break;
     case 1:
         _mcpwm_set(1, fre, duty);
+        mcpwm_att(1);
         break;
     case 2:
         _mcpwm_set(2, fre, duty);
+        mcpwm_att(2);
         break;
     case 3:
         _mcpwm_set(3, fre, duty);
+        mcpwm_att(3);
         break;
     default:
         break;
